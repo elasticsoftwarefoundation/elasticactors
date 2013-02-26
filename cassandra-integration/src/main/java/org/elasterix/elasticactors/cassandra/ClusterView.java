cb4033fa8588503c9dc3f5724e69d26a4581f8a4
Joost van de Wijgerd <jwijgerd@gmail.com>
2013-02-26 14:27:54 +0100

    Initial commit, added Apache 2.0 license. Added Spring 3.2 and AspectJ 1.7 libraries. Also linking to Cassandra 1.2

---
+++ cassandra-integration/src/main/java/org/elasterix/elasticactors/cassandra/ClusterView.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2013 Joost van de Wijgerd <jwijgerd@gmail.com>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.elasterix.elasticactors.cassandra;
+
+import org.apache.cassandra.service.IEndpointLifecycleSubscriber;
+
+import java.net.InetAddress;
+
+/**
+ *
+ */
+public class ClusterView implements IEndpointLifecycleSubscriber {
+    @Override
+    public void onJoinCluster(InetAddress endpoint) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void onLeaveCluster(InetAddress endpoint) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void onUp(InetAddress endpoint) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void onDown(InetAddress endpoint) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void onMove(InetAddress endpoint) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
