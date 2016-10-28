8edb128f381ded37ada6bba5136a3cadfb467f64
jwijgerd <jwijgerd@gmail.com>
2016-10-28 18:38:23 +0200

    updated copyright statements and added a reactive-streams module that will implement the reactive-streams speficication on top of ElasticActors

---
+++ reactive-streams/src/main/java/org/elasticsoftware/elasticactors/reactivestreams/ReactiveStream.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2013 - 2016 The Original Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.elasticsoftware.elasticactors.reactivestreams;
+
+import org.elasticsoftware.elasticactors.ActorRef;
+import org.elasticsoftware.elasticactors.ActorSystem;
+import org.elasticsoftware.elasticactors.reactivestreams.protocol.Subscribe;
+import org.reactivestreams.Publisher;
+
+/**
+ * @author Joost van de Wijgerd
+ */
+public final class ReactiveStream {
+    public static <T> Publisher<T> publisherOf(ActorSystem actorSystem, String actorId) {
+        return s -> {
+            try {
+                ActorRef publisher = actorSystem.actorFor(actorId);
+                ActorRef subscriber = actorSystem.tempActorOf(SubscriberActor.class, new SubscriberActorDelegate<>(s, publisher));
+                publisher.tell(new Subscribe(), subscriber);
+            } catch(Exception e) {
+                s.onError(e);
+            }
+        };
+    }
+    public static <T> Publisher<T> publisherOf(ActorSystem actorSystem, String actorId, Object subscriptionMessage) {
+        return s -> {
+            try {
+                ActorRef publisher = actorSystem.actorFor(actorId);
+                ActorRef subscriber = actorSystem.tempActorOf(SubscriberActor.class, new SubscriberActorDelegate<>(s, publisher));
+                publisher.tell(subscriptionMessage, subscriber);
+            } catch(Exception e) {
+                s.onError(e);
+            }
+        };
+    }
+}
---
+++ reactive-streams/src/test/java/org/elasticsoftware/elasticactors/reactivestreams/TestPublishingActor.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2013 - 2016 The Original Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.elasticsoftware.elasticactors.reactivestreams;
+
+import org.elasticsoftware.elasticactors.Actor;
+import org.elasticsoftware.elasticactors.ActorSystem;
+import org.elasticsoftware.elasticactors.MessageHandler;
+import org.elasticsoftware.elasticactors.base.serialization.JacksonSerializationFramework;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Joost van de Wijgerd
+ */
+@Actor(stateClass = TestPublishingActorState.class, serializationFramework = JacksonSerializationFramework.class)
+public final class TestPublishingActor extends PublishingActor {
+
+
+    @Override
+    public void postActivate(String previousVersion) throws Exception {
+        getSystem().getScheduler().scheduleOnce(getSelf(), new GenerateTestMessages(1), getSelf(), 1, TimeUnit.SECONDS);
+    }
+
+    @MessageHandler
+    public void handle(GenerateTestMessages message, ActorSystem actorSystem) {
+        publish(message);
+        getSystem().getScheduler().scheduleOnce(getSelf(), new GenerateTestMessages(message.getTotal()+1), getSelf(), 1, TimeUnit.SECONDS);
+    }
+}
