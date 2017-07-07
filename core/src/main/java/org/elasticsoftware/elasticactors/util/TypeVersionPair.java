d2188526710a721f1cf13496803918d9fd3796b4
jwijgerd <jwijgerd@gmail.com>
2017-07-07 10:27:24 +0200

    forgot to add file to git

---
+++ core/src/main/java/org/elasticsoftware/elasticactors/util/TypeVersionPair.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2013 - 2017 The Original Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.elasticsoftware.elasticactors.util;
+
+/**
+ * @author Joost van de Wijgerd
+ */
+public final class TypeVersionPair {
+    private final String type;
+    private final String version;
+
+    public TypeVersionPair(String type, String version) {
+        this.type = type;
+        this.version = version;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TypeVersionPair that = (TypeVersionPair) o;
+
+        if (!type.equals(that.type)) return false;
+        return version.equals(that.version);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = type.hashCode();
+        result = 31 * result + version.hashCode();
+        return result;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+}
