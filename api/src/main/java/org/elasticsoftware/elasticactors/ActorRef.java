8831d3217863b8fd90cba7e43e23f0bd7ddff5dd
Joost van de Wijgerd <jwijgerd@gmail.com>
2017-10-02 15:12:01 +0200

    added missing javadoc

--- api/src/main/java/org/elasticsoftware/elasticactors/ActorRef.java
+++ api/src/main/java/org/elasticsoftware/elasticactors/ActorRef.java
@@ -88,6 +88,16 @@ public interface ActorRef {
      */
     <T> CompletionStage<T> ask(Object message, Class<T> responseType);
 
+    /**
+     * Send a message to an {@link ElasticActor} and request a response. when {@param persistOnResponse} is {@link Boolean#TRUE}
+     * the calling actor's state (if it's a persistent actor) will be saved.
+     *
+     * @param message
+     * @param responseType
+     * @param persistOnResponse
+     * @param <T>
+     * @return
+     */
     <T> CompletionStage<T> ask(Object message, Class<T> responseType, Boolean persistOnResponse);
 
     /**
