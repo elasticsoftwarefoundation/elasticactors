e365adced3024eb6eaad328129e71c62156abceb
Andre Brait <andrebrait@gmail.com>
2020-03-03 15:51:50 +0100

    Remove unnecessary separators

--- api/src/main/java/org/elasticsoftware/elasticactors/MethodActor.java
+++ api/src/main/java/org/elasticsoftware/elasticactors/MethodActor.java
@@ -189,8 +189,8 @@ public abstract class MethodActor extends TypedActor<Object> implements Persiste
     protected void onUnhandled(ActorRef sender, Object message) {
         logger.error(
                 "Unhandled message of type [{}] received. "
-                        + "Actor: [{}]. "
-                        + "Sender: [{}]. "
+                        + "Actor [{}]. "
+                        + "Sender [{}]. "
                         + "Message payload: {}",
                 message.getClass().getName(),
                 getSelf(),
