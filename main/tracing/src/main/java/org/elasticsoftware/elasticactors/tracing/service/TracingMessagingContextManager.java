376ebd0d1def85b79386f2f18f7a4de04203d030
Andre Brait <andrebrait@gmail.com>
2021-11-08 19:37:35 +0100

    Add initial optimized tracing for log4j2

--- main/tracing/src/main/java/org/elasticsoftware/elasticactors/tracing/service/TracingMessagingContextManager.java
+++ main/tracing/src/main/java/org/elasticsoftware/elasticactors/tracing/service/TracingMessagingContextManager.java
@@ -2,6 +2,7 @@ package org.elasticsoftware.elasticactors.tracing.service;
 
 import org.elasticsoftware.elasticactors.ActorContext;
 import org.elasticsoftware.elasticactors.tracing.CreationContext;
+import org.elasticsoftware.elasticactors.tracing.LogContextProcessor;
 import org.elasticsoftware.elasticactors.tracing.MessageHandlingContext;
 import org.elasticsoftware.elasticactors.tracing.MessagingContextManager;
 import org.elasticsoftware.elasticactors.tracing.NoopMessagingScope;
@@ -15,8 +16,11 @@ import javax.annotation.Nullable;
 import java.lang.reflect.Method;
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.ServiceLoader;
 import java.util.StringJoiner;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Function;
@@ -28,6 +32,44 @@ import static java.lang.ThreadLocal.withInitial;
 public final class TracingMessagingContextManager extends MessagingContextManager {
 
     private final static ThreadLocal<Deque<MessagingScope>> scopes = withInitial(ArrayDeque::new);
+
+    /*
+     * Initialization-on-deman holder pattern (lazy-loaded singleton)
+     * See: https://en.wikipedia.org/wiki/Initialization-on-demand_holder_idiom
+     */
+    private static final class LogContextProcessorHolder {
+
+        private static final LogContextProcessor INSTANCE = loadService();
+
+        private static LogContextProcessor loadService() {
+            try {
+                return Optional.of(ServiceLoader.load(LogContextProcessor.class))
+                    .map(ServiceLoader::iterator)
+                    .filter(Iterator::hasNext)
+                    .map(LogContextProcessorHolder::loadFirst)
+                    .orElseGet(() -> {
+                        logger.warn(
+                            "No implementations of LogContextProcessor were found. "
+                                + "Falling back to the unoptimized default.");
+                        return new Slf4jMDCLogContexProcessor();
+                    });
+            } catch (Exception e) {
+                logger.error(
+                    "Exception thrown while loading LogContextProcessor implementation. "
+                        + "Falling back to the unoptimized default.", e);
+                return new Slf4jMDCLogContexProcessor();
+            }
+        }
+
+        private static LogContextProcessor loadFirst(Iterator<LogContextProcessor> iter) {
+            LogContextProcessor service = iter.next();
+            logger.info(
+                "Loaded LogContextProcessor implementation [{}]",
+                service.getClass().getName()
+            );
+            return service;
+        }
+    }
 
     @Override
     public boolean isTracingEnabled() {
@@ -277,140 +319,148 @@ public final class TracingMessagingContextManager extends MessagingContextManage
         @Nullable MessagingScope current,
         @Nullable MessagingScope next)
     {
-        TraceContext currentTraceContext = null;
-        CreationContext currentCreationContext = null;
-        MessageHandlingContext currentMessagingContext = null;
-        Method currentMethod = null;
-
-        TraceContext nextTraceContext = null;
-        CreationContext nextCreationContext = null;
-        MessageHandlingContext nextMessagingContext = null;
-        Method nextMethod = null;
-
-        if (current != null) {
-            currentTraceContext = current.getTraceContext();
-            currentCreationContext = current.getCreationContext();
-            currentMessagingContext = current.getMessageHandlingContext();
-            currentMethod = current.getMethod();
-        }
-
-        if (next != null) {
-            nextTraceContext = next.getTraceContext();
-            nextCreationContext = next.getCreationContext();
-            nextMessagingContext = next.getMessageHandlingContext();
-            nextMethod = next.getMethod();
-        }
-
-        if (currentTraceContext != nextTraceContext) {
-            fillContext(currentTraceContext, nextTraceContext);
-        }
-        if (currentCreationContext != nextCreationContext) {
-            fillContext(currentCreationContext, nextCreationContext);
-        }
-        if (currentMessagingContext != nextMessagingContext) {
-            fillContext(currentMessagingContext, nextMessagingContext);
-        }
-        if (currentMethod != nextMethod) {
-            fillContext(nextMethod);
-        }
+        LogContextProcessorHolder.INSTANCE.process(current, next);
     }
 
-    private static void fillContext(@Nullable TraceContext current, @Nullable TraceContext next) {
-        updateBaggage(current, next);
-        addToLogContext(SPAN_ID_KEY, current, next, TraceContext::getSpanId);
-        addToLogContext(TRACE_ID_KEY, current, next, TraceContext::getTraceId);
-        addToLogContext(PARENT_SPAN_ID_KEY, current, next, TraceContext::getParentId);
-    }
+    private final static class Slf4jMDCLogContexProcessor implements LogContextProcessor {
 
-    private static void updateBaggage(TraceContext current, TraceContext next) {
-        try {
-            Map<String, String> oldBaggage = current != null ? current.getBaggage() : null;
-            Map<String, String> nextBaggage = next != null ? next.getBaggage() : null;
-            if (oldBaggage == nextBaggage) {
-                return;
+        @Override
+        public void process(@Nullable MessagingScope current, @Nullable MessagingScope next) {
+            TraceContext currentTraceContext = null;
+            CreationContext currentCreationContext = null;
+            MessageHandlingContext currentMessagingContext = null;
+            Method currentMethod = null;
+
+            TraceContext nextTraceContext = null;
+            CreationContext nextCreationContext = null;
+            MessageHandlingContext nextMessagingContext = null;
+            Method nextMethod = null;
+
+            if (current != null) {
+                currentTraceContext = current.getTraceContext();
+                currentCreationContext = current.getCreationContext();
+                currentMessagingContext = current.getMessageHandlingContext();
+                currentMethod = current.getMethod();
             }
-            if (oldBaggage == null) {
-                nextBaggage.forEach(TracingMessagingContextManager::putOnMDC);
-            } else if (nextBaggage == null) {
-                oldBaggage.keySet().forEach(key -> putOnMDC(key, null));
+
+            if (next != null) {
+                nextTraceContext = next.getTraceContext();
+                nextCreationContext = next.getCreationContext();
+                nextMessagingContext = next.getMessageHandlingContext();
+                nextMethod = next.getMethod();
+            }
+
+            if (currentTraceContext != nextTraceContext) {
+                fillContext(currentTraceContext, nextTraceContext);
+            }
+            if (currentCreationContext != nextCreationContext) {
+                fillContext(currentCreationContext, nextCreationContext);
+            }
+            if (currentMessagingContext != nextMessagingContext) {
+                fillContext(currentMessagingContext, nextMessagingContext);
+            }
+            if (currentMethod != nextMethod) {
+                fillContext(nextMethod);
+            }
+        }
+
+        private static void fillContext(@Nullable TraceContext current, @Nullable TraceContext next) {
+            updateBaggage(current, next);
+            addToLogContext(SPAN_ID_KEY, current, next, TraceContext::getSpanId);
+            addToLogContext(TRACE_ID_KEY, current, next, TraceContext::getTraceId);
+            addToLogContext(PARENT_SPAN_ID_KEY, current, next, TraceContext::getParentId);
+        }
+
+        private static void updateBaggage(TraceContext current, TraceContext next) {
+            try {
+                Map<String, String> oldBaggage = current != null ? current.getBaggage() : null;
+                Map<String, String> nextBaggage = next != null ? next.getBaggage() : null;
+                if (oldBaggage == nextBaggage) {
+                    return;
+                }
+                if (oldBaggage == null) {
+                    nextBaggage.forEach(Slf4jMDCLogContexProcessor::putOnMDC);
+                } else if (nextBaggage == null) {
+                    oldBaggage.keySet().forEach(key -> putOnMDC(key, null));
+                } else {
+                    oldBaggage.forEach((key, value) -> {
+                        String nextValue = nextBaggage.get(key);
+                        // Will set the new value if present, and remove it if absent
+                        if (!Objects.equals(value, nextValue)) {
+                            putOnMDC(key, nextValue);
+                        }
+                    });
+                    nextBaggage.forEach((key, value) -> {
+                        String oldValue = oldBaggage.get(key);
+                        // Would have been processed when processing oldBaggage
+                        if (oldValue != null) {
+                            return;
+                        }
+                        // Only insert if the old value was null and the new one isn't
+                        if (value != null) {
+                            putOnMDC(key, value);
+                        }
+                    });
+                }
+            } catch (Exception e) {
+                logger.error(
+                    "Could not add trace baggage to the MDC. "
+                        + "Baggage fields on the MDC might be corrupted.",
+                    e
+                );
+            }
+        }
+
+        private static void fillContext(
+            @Nullable MessageHandlingContext current,
+            @Nullable MessageHandlingContext next)
+        {
+            addToLogContext(MESSAGE_TYPE_KEY, current, next, MessageHandlingContext::getMessageType);
+            addToLogContext(SENDER_KEY, current, next, MessageHandlingContext::getSender);
+            addToLogContext(RECEIVER_KEY, current, next, MessageHandlingContext::getReceiver);
+            addToLogContext(RECEIVER_TYPE_KEY, current, next, MessageHandlingContext::getReceiverType);
+        }
+
+        private static void fillContext(
+            @Nullable CreationContext current,
+            @Nullable CreationContext next)
+        {
+            addToLogContext(CREATOR_KEY, current, next, CreationContext::getCreator);
+            addToLogContext(CREATOR_TYPE_KEY, current, next, CreationContext::getCreatorType);
+            addToLogContext(CREATOR_METHOD_KEY, current, next, CreationContext::getCreatorMethod);
+            addToLogContext(SCHEDULED_KEY, current, next, CreationContext::getScheduled);
+        }
+
+        private static void fillContext(@Nullable Method next) {
+            putOnMDC(RECEIVER_METHOD_KEY, getValue(next, TracingUtils::shorten));
+        }
+
+        private static <D, T> void addToLogContext(
+            @Nonnull String key,
+            @Nullable D oldObject,
+            @Nullable D newObject,
+            @Nonnull Function<D, T> getterFunction)
+        {
+            String oldValue = getValue(oldObject, getterFunction);
+            String newValue = getValue(newObject, getterFunction);
+            if (!(Objects.equals(oldValue, newValue))) {
+                putOnMDC(key, newValue);
+            }
+        }
+
+        private static void putOnMDC(@Nonnull String key, String newValue) {
+            if (newValue != null) {
+                MDC.put(key, newValue);
             } else {
-                oldBaggage.forEach((key, value) -> {
-                    String nextValue = nextBaggage.get(key);
-                    // Will set the new value if present, and remove it if absent
-                    if (!Objects.equals(value, nextValue)) {
-                        putOnMDC(key, nextValue);
-                    }
-                });
-                nextBaggage.forEach((key, value) -> {
-                    String oldValue = oldBaggage.get(key);
-                    // Would have been processed when processing oldBaggage
-                    if (oldValue != null) {
-                        return;
-                    }
-                    // Only insert if the old value was null and the new one isn't
-                    if (value != null) {
-                        putOnMDC(key, value);
-                    }
-                });
+                MDC.remove(key);
             }
-        } catch (Exception e) {
-            logger.error(
-                "Could not add trace baggage to the MDC. "
-                    + "Baggage fields on the MDC might be corrupted.",
-                e
-            );
         }
-    }
 
-    private static void fillContext(
-        @Nullable MessageHandlingContext current,
-        @Nullable MessageHandlingContext next)
-    {
-        addToLogContext(MESSAGE_TYPE_KEY, current, next, MessageHandlingContext::getMessageType);
-        addToLogContext(SENDER_KEY, current, next, MessageHandlingContext::getSender);
-        addToLogContext(RECEIVER_KEY, current, next, MessageHandlingContext::getReceiver);
-        addToLogContext(RECEIVER_TYPE_KEY, current, next, MessageHandlingContext::getReceiverType);
-    }
-
-    private static void fillContext(
-        @Nullable CreationContext current,
-        @Nullable CreationContext next)
-    {
-        addToLogContext(CREATOR_KEY, current, next, CreationContext::getCreator);
-        addToLogContext(CREATOR_TYPE_KEY, current, next, CreationContext::getCreatorType);
-        addToLogContext(CREATOR_METHOD_KEY, current, next, CreationContext::getCreatorMethod);
-        addToLogContext(SCHEDULED_KEY, current, next, CreationContext::getScheduled);
-    }
-
-    private static void fillContext(@Nullable Method next) {
-        putOnMDC(RECEIVER_METHOD_KEY, getValue(next, TracingUtils::shorten));
-    }
-
-    private static <D, T> void addToLogContext(
-        @Nonnull String key,
-        @Nullable D oldObject,
-        @Nullable D newObject,
-        @Nonnull Function<D, T> getterFunction)
-    {
-        String oldValue = getValue(oldObject, getterFunction);
-        String newValue = getValue(newObject, getterFunction);
-        if (!(Objects.equals(oldValue, newValue))) {
-            putOnMDC(key, newValue);
+        private static <D, T> String getValue(
+            @Nullable D oldObject,
+            @Nonnull Function<D, T> getterFunction)
+        {
+            return oldObject != null ? safeToString(getterFunction.apply(oldObject)) : null;
         }
-    }
-
-    private static void putOnMDC(@Nonnull String key, String newValue) {
-        if (newValue != null) {
-            MDC.put(key, newValue);
-        } else {
-            MDC.remove(key);
-        }
-    }
-
-    private static <D, T> String getValue(
-        @Nullable D oldObject,
-        @Nonnull Function<D, T> getterFunction)
-    {
-        return oldObject != null ? safeToString(getterFunction.apply(oldObject)) : null;
     }
 }
