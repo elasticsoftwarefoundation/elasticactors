dbb563e97fabec7bf0dbe86487d3acf9ffd602db
Andre Brait <andrebrait@gmail.com>
2021-11-10 15:51:18 +0100

    Default MetricsSettings values to 0

--- main/core/src/main/java/org/elasticsoftware/elasticactors/cluster/metrics/MetricsSettings.java
+++ main/core/src/main/java/org/elasticsoftware/elasticactors/cluster/metrics/MetricsSettings.java
@@ -3,19 +3,19 @@ package org.elasticsoftware.elasticactors.cluster.metrics;
 public final class MetricsSettings {
 
     private final boolean enabled;
-    private final Long messageDeliveryWarnThreshold;
-    private final Long messageHandlingWarnThreshold;
-    private final Long serializationWarnThreshold;
+    private final long messageDeliveryWarnThreshold;
+    private final long messageHandlingWarnThreshold;
+    private final long serializationWarnThreshold;
 
     public static MetricsSettings disabled() {
-        return new MetricsSettings(false, null, null, null);
+        return new MetricsSettings(false, 0L, 0L, 0L);
     }
 
     public MetricsSettings(
         boolean enabled,
-        Long messageDeliveryWarnThreshold,
-        Long messageHandlingWarnThreshold,
-        Long serializationWarnThreshold)
+        long messageDeliveryWarnThreshold,
+        long messageHandlingWarnThreshold,
+        long serializationWarnThreshold)
     {
         this.enabled = enabled;
         this.messageDeliveryWarnThreshold = messageDeliveryWarnThreshold;
@@ -28,31 +28,27 @@ public final class MetricsSettings {
     }
 
     public boolean isMessageDeliveryWarnThresholdEnabled() {
-        return enabled && messageDeliveryWarnThreshold != null && messageDeliveryWarnThreshold > 0L;
+        return enabled && messageDeliveryWarnThreshold > 0L;
     }
 
     public long getMessageDeliveryWarnThreshold() {
-        return getLongValue(messageDeliveryWarnThreshold);
+        return messageDeliveryWarnThreshold;
     }
 
     public boolean isMessageHandlingWarnThresholdEnabled() {
-        return enabled && messageHandlingWarnThreshold != null && messageHandlingWarnThreshold > 0L;
+        return enabled && messageHandlingWarnThreshold > 0L;
     }
 
     public long getMessageHandlingWarnThreshold() {
-        return getLongValue(messageHandlingWarnThreshold);
+        return messageHandlingWarnThreshold;
     }
 
     public boolean isSerializationWarnThresholdEnabled() {
-        return enabled && serializationWarnThreshold != null && serializationWarnThreshold > 0L;
+        return enabled && serializationWarnThreshold > 0L;
     }
 
     public long getSerializationWarnThreshold() {
-        return getLongValue(serializationWarnThreshold);
-    }
-
-    private static long getLongValue(Long number) {
-        return number != null ? number : 0L;
+        return serializationWarnThreshold;
     }
 
     public boolean requiresMeasurement() {
