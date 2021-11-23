7c695759c808bc648fa4d72962b75f91e77a2163
Andre Brait <andrebrait@gmail.com>
2021-11-23 13:03:24 +0100

    Fix scanning Jackson's own serializers as custom

--- main/base/src/main/java/org/elasticsoftware/elasticactors/base/serialization/ObjectMapperBuilder.java
+++ main/base/src/main/java/org/elasticsoftware/elasticactors/base/serialization/ObjectMapperBuilder.java
@@ -20,10 +20,12 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.deser.std.StdScalarDeserializer;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.StdScalarSerializer;
 import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
 import net.jodah.typetools.TypeResolver;
 import org.elasticsoftware.elasticactors.cluster.ActorRefFactory;
@@ -31,17 +33,21 @@ import org.elasticsoftware.elasticactors.cluster.scheduler.ScheduledMessageRefFa
 import org.reflections.Reflections;
 import org.reflections.util.ClasspathHelper;
 import org.reflections.util.ConfigurationBuilder;
+import org.reflections.util.FilterBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Properties;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * {@link com.fasterxml.jackson.annotation.JsonTypeName}. The classes are registered as subtypes
@@ -99,26 +105,40 @@ public class ObjectMapperBuilder {
             basePackages.addAll(Arrays.asList(otherPackages));
         }
 
+        logger.info("Building ObjectMapper");
         ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+
+        logger.debug("Scanning the following packages: {}", basePackages);
 
         for (String basePackage : basePackages) {
             configurationBuilder.addUrls(ClasspathHelper.forPackage(basePackage));
         }
 
+        FilterBuilder filterBuilder = new FilterBuilder();
+        filterBuilder.excludePackage(JsonSerializer.class.getPackage().getName());
+        filterBuilder.excludePackage(StdScalarSerializer.class.getPackage().getName());
+        filterBuilder.excludePackage(JsonDeserializer.class.getPackage().getName());
+        filterBuilder.excludePackage(StdScalarDeserializer.class.getPackage().getName());
+
+        configurationBuilder.filterInputsBy(filterBuilder);
+
         Reflections reflections = new Reflections(configurationBuilder);
         registerSubtypes(reflections, objectMapper);
 
-        SimpleModule jacksonModule = new SimpleModule("org.elasticsoftware.elasticactors",new Version(1,0,0,null,null,null));
+        SimpleModule elasticActorsModule =
+            new SimpleModule("ElasticActorsModule", new Version(1, 0, 0, null, null, null));
 
-        registerCustomSerializers(reflections,jacksonModule);
-        registerCustomDeserializers(reflections,jacksonModule);
+        registerCustomSerializers(reflections,elasticActorsModule);
+        registerCustomDeserializers(reflections,elasticActorsModule);
 
-        objectMapper.registerModule(jacksonModule);
+        logger.info("Registering Jackson module: {}", elasticActorsModule.getModuleName());
+        objectMapper.registerModule(elasticActorsModule);
 
         if(useAfterBurner) {
             // register the afterburner module to increase performance
             // afterburner does NOT work correctly with jackson version lower than 2.4.5! (if @JsonSerialize annotation is used)
             AfterburnerModule afterburnerModule = new AfterburnerModule();
+            logger.info("Registering Jackson module: {}", afterburnerModule.getModuleName());
             //afterburnerModule.setUseValueClassLoader(false);
             //afterburnerModule.setUseOptimizedBeanDeserializer(false);
             objectMapper.registerModule(afterburnerModule);
@@ -128,16 +148,42 @@ public class ObjectMapperBuilder {
     }
 
     private void registerSubtypes(Reflections reflections,ObjectMapper objectMapper) {
+        logger.info("Scanning @JsonTypeName-annotated classes");
         Set<Class<?>> subTypes = reflections.getTypesAnnotatedWith(JsonTypeName.class);
+        logger.info("Found {} classes annotated with @JsonTypeName", subTypes.size());
+        if (logger.isDebugEnabled()) {
+            logger.debug(
+                "Found the following classes annotated with @JsonTypeName: {}",
+                subTypes.stream().map(Class::getName).collect(Collectors.toList())
+            );
+        }
         objectMapper.registerSubtypes(subTypes.toArray(new Class<?>[0]));
     }
 
     private void registerCustomSerializers(Reflections reflections, SimpleModule jacksonModule) {
-        Set<Class<? extends JsonSerializer>> customSerializers = reflections.getSubTypesOf(JsonSerializer.class);
+        logger.info("Scanning classes that extend JsonSerializer");
+        List<Class<? extends JsonSerializer>> customSerializers =
+            reflections.getSubTypesOf(JsonSerializer.class)
+                .stream()
+                .filter(ObjectMapperBuilder::isInstantiable)
+                .collect(Collectors.toList());
+        logger.info("Found {} classes that extend JsonSerializer", customSerializers.size());
+        if (logger.isDebugEnabled()) {
+            logger.debug(
+                "Found the following classes extending JsonSerializer: {}",
+                customSerializers.stream().map(Class::getName).collect(Collectors.toList())
+            );
+        }
         for (Class<? extends JsonSerializer> customSerializer : customSerializers) {
             Class<?> objectClass = TypeResolver.resolveRawArgument(JsonSerializer.class,customSerializer);
             try {
-                jacksonModule.addSerializer(objectClass, customSerializer.newInstance());
+                JsonSerializer jsonSerializer = customSerializer.newInstance();
+                logger.debug(
+                    "Adding serializer [{}] for type [{}]",
+                    customSerializer.getName(),
+                    objectClass.getName()
+                );
+                jacksonModule.addSerializer(objectClass, jsonSerializer);
             } catch(Exception e) {
                 logger.error("Failed to create Custom Jackson Serializer: {}",customSerializer.getName(),e);
             }
@@ -146,13 +192,30 @@ public class ObjectMapperBuilder {
 
     private void registerCustomDeserializers(Reflections reflections, SimpleModule jacksonModule) {
         // @todo: looks like the SubTypeScanner only goes one level deep, need to fix this
-        Set<Class<? extends StdScalarDeserializer>> customDeserializers = reflections.getSubTypesOf(StdScalarDeserializer.class);
+        logger.info("Scanning classes that extend StdScalarDeserializer");
+        List<Class<? extends StdScalarDeserializer>> customDeserializers =
+            reflections.getSubTypesOf(StdScalarDeserializer.class)
+                .stream()
+                .filter(ObjectMapperBuilder::isInstantiable)
+                .collect(Collectors.toList());
+        logger.info("Found {} classes that extend StdScalarDeserializer", customDeserializers.size());
+        if (logger.isDebugEnabled()) {
+            logger.debug(
+                "Found the following classes extending StdScalarDeserializer: {}",
+                customDeserializers.stream().map(Class::getName).collect(Collectors.toList())
+            );
+        }
         for (Class<? extends StdScalarDeserializer> customDeserializer : customDeserializers) {
             // need to exclude the JacksonActorRefDeserializer
             if(hasNoArgConstructor(customDeserializer)) {
                 try {
                     StdScalarDeserializer deserializer = customDeserializer.newInstance();
                     Class<?> objectClass = deserializer.handledType();
+                    logger.debug(
+                        "Adding deserializer [{}] for type [{}]",
+                        customDeserializer.getName(),
+                        objectClass.getName()
+                    );
                     jacksonModule.addDeserializer(objectClass, deserializer);
                 } catch(Exception e) {
                     logger.error("Failed to create Custom Jackson Deserializer: {}", customDeserializer.getName(), e);
@@ -164,6 +227,11 @@ public class ObjectMapperBuilder {
                         try {
                             StdScalarDeserializer deserializer = (StdScalarDeserializer) constructor.newInstance(actorRefFactory);
                             Class<?> objectClass = deserializer.handledType();
+                            logger.debug(
+                                "Adding deserializer [{}] for type [{}]",
+                                customDeserializer.getName(),
+                                objectClass.getName()
+                            );
                             jacksonModule.addDeserializer(objectClass, deserializer);
                             break;
                         } catch(Exception e) {
@@ -173,16 +241,33 @@ public class ObjectMapperBuilder {
                         try {
                             StdScalarDeserializer deserializer = (StdScalarDeserializer) constructor.newInstance(scheduledMessageRefFactory);
                             Class<?> objectClass = deserializer.handledType();
+                            logger.debug(
+                                "Adding deserializer [{}] for type [{}]",
+                                customDeserializer.getName(),
+                                objectClass.getName()
+                            );
                             jacksonModule.addDeserializer(objectClass, deserializer);
                             break;
                         } catch(Exception e) {
                             logger.error("Failed to create Custom Jackson Deserializer: {}", customDeserializer.getName(),e);
                         }
+                    } else {
+                        logger.debug(
+                            "Could not find a suitable constructor for deserializer [{}]",
+                            customDeserializer.getName()
+                        );
                     }
                 }
 
             }
         }
+    }
+
+    private static boolean isInstantiable(Class<?> aClass) {
+        return !aClass.isInterface()
+            && !aClass.isAnonymousClass()
+            && !aClass.isEnum()
+            && !Modifier.isAbstract(aClass.getModifiers());
     }
 
     private boolean hasNoArgConstructor(Class<? extends StdScalarDeserializer> customDeserializer) {
